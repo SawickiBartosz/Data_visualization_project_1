d786ddd524a9f1fcf2039b9ba52c86b3261802ee
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2020-11-14 01:04:56 +0100

    cleenup

--- projekt1/propSymbolsLayer_world_map_population.R
+++
@@ -1,9 +0,0 @@
-library(cartography)
-library(sf)
-
-plot(st_geometry(world))
-propSymbolsLayer(world,var = "pop",
-                 col = "#756bb1", 
-                 legend.pos = c(-180,-50),
-                 legend.values.rnd = -4, 
-                 legend.title.txt = "Population by countries")
